436095c6b43a1eee5864c4eea8e0215a7b0b4ca0
KateChou <akak37.tw@gmail.com>
2026-03-25 09:19:39 +0800

    Fix: 修正vite基礎路徑以符合倉庫名稱

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/r-homework-week2/', // 設定為你的倉庫名稱
+  base: '/homework-week2/', // 設定為你的倉庫名稱
   plugins: [react()],
   server: {
     open: true,
