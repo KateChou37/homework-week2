6cf23bc2a98c502a8dd1e1794d95e1673b9444ee
KateChou <akak37.tw@gmail.com>
2026-03-25 09:30:17 +0800

    Fix: 更新vite基礎路徑以根據環境變數設定

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/homework-week2/', // 設定為你的倉庫名稱
+   base: process.env.NODE_ENV === 'production'
+    ? '/homework-week2/'
+    : '/',// 設定為你的倉庫名稱
   plugins: [react()],
   server: {
     open: true,
